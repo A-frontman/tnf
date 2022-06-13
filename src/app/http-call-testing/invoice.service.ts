dbc25841432493960d86fa78626ecd5eb070e753
Mateusz Gawlik <kontakt@a-frontman.pl>
2022-06-13 20:12:42 +0200

    HttpCallTest: Should throw error, when no body in response (but test fails... RACE CONDITION )

--- src/app/http-call-testing/invoice.service.ts
+++ src/app/http-call-testing/invoice.service.ts
@@ -16,6 +16,7 @@ export class InvoiceService {
 		this.httpClient.get<HttpResponse<{ list: Invoice[] }>>(`${ environment.api }/${ userId }`).subscribe(
 			(response) => {
 
+				// Some complicated logic here, that can cause an error to be thrown
 				if (!response.body) {
 					throw 'INCORRECT_RESPONSE'
 				}
