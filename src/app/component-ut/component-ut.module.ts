2c12a646f2dec37309ae350f5182c1ba7e788611
Mateusz Gawlik <kontakt@a-frontman.pl>
2022-06-14 08:23:50 +0200

    Angular material added

---
+++ src/app/component-ut/component-ut.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StateComponent } from './state/state.component';
+
+
+@NgModule({
+	declarations: [
+    StateComponent
+  ],
+	imports: [
+		CommonModule
+	]
+})
+export class ComponentUtModule {
+}
